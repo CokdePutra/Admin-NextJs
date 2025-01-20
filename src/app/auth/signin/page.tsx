632af3c0436444757e1df9b545164b3574c02b21
I Made Kanha Mahesyogi <kmahesyogi@gmail.com>
2025-01-20 16:44:50 +0800

    membuat signin

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Signin from "@/components/Auth/Signin";
 
 export const metadata: Metadata = {
@@ -13,53 +11,52 @@ export const metadata: Metadata = {
 
 const SignIn: React.FC = () => {
   return (
-<>
-
-      <div className="rounded-[10px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+      <div className="max-w-[800px] mx-auto rounded-[5px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card">
         <div className="flex flex-wrap items-center">
           <div className="w-full xl:w-1/2">
-            <div className="w-full p-4 sm:p-12.5 xl:p-15">
+            <div className="w-full p-10 sm:p-8 xl:p-10">
               <Signin />
             </div>
           </div>
 
-          <div className="hidden w-full p-7.5 xl:block xl:w-1/2">
-            <div className="custom-gradient-1 overflow-hidden rounded-2xl px-12.5 pt-12.5 dark:!bg-dark-2 dark:bg-none">
-              <Link className="mb-10 inline-block" href="/">
+          <div className="hidden w-full p-5 xl:block xl:w-1/2">
+            <div className="custom-gradient-1 overflow-hidden rounded-2xl px-8 pt-8 dark:!bg-dark-2 dark:bg-none">
+              <Link className="mb-8 inline-block" href="/">
                 <Image
                   className="hidden dark:block"
                   src={"/images/logo/logo.svg"}
                   alt="Logo"
-                  width={176}
-                  height={32}
+                  width={140}
+                  height={26}
                 />
                 <Image
                   className="dark:hidden"
                   src={"/images/logo/logo-dark.svg"}
                   alt="Logo"
-                  width={176}
-                  height={32}
+                  width={140}
+                  height={26}
                 />
               </Link>
-              <p className="mb-3 text-xl font-medium text-dark dark:text-white">
+              <p className="mb-2 text-lg font-medium text-dark dark:text-white">
                 Sign in to your account
               </p>
 
-              <h1 className="mb-4 text-2xl font-bold text-dark dark:text-white sm:text-heading-3">
+              <h1 className="mb-3 text-xl font-bold text-dark dark:text-white sm:text-heading-3">
                 Welcome Back!
               </h1>
 
-              <p className="w-full max-w-[375px] font-medium text-dark-4 dark:text-dark-6">
+              <p className="w-full max-w-[300px] font-medium text-dark-4 dark:text-dark-6">
                 Please sign in to your account by completing the necessary
                 fields below
               </p>
 
-              <div className="mt-31">
+              <div className="mt-20">
                 <Image
                   src={"/images/grids/grid-02.svg"}
                   alt="Logo"
-                  width={405}   
-                  height={325}
+                  width={320}
+                  height={260}
                   className="mx-auto dark:opacity-30"
                 />
               </div>
@@ -67,7 +64,8 @@ const SignIn: React.FC = () => {
           </div>
         </div>
       </div>
-      </>
+    </div>
+    
   );
 };
 
