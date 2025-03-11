b6abdf94855c58bef756b6cf3dc5905fc601179d
CokdePutra <cokgdeputrawidnyana25@gmail.com>
2025-03-11 17:45:24 +0800

    creating profile for user

--- src/components/User/Kegiatan/Kegiatan.tsx
+++ src/components/User/Kegiatan/Kegiatan.tsx
@@ -25,11 +25,7 @@ import axios from 'axios';
           title: 'MOCA',
           description: 'Mountain Camping merupakan kegiatan rutin unit dalam memberikan bantuan pengambilan atau pemberian ilmu di dataran tinggi',
           image: '/kegiatan-photo/Moca-png.png',
-        }, {
-            title: 'MOCA',
-            description: 'Mountain Camping merupakan kegiatan rutin unit dalam memberikan bantuan pengambilan atau pemberian ilmu di dataran tinggi',
-            image: '/kegiatan-photo/Moca-png.png',
-          },
+        },
         
       ]
 
@@ -40,16 +36,19 @@ const Kegiatan: React.FC = () => {
           <h2 className="text-4xl font-bold text-center mb-2 text-black">KEGIATAN</h2>
           <h2 className="text-2xl text-center mb-10 text-black">KSR ITB STIKOM Bali</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-        {activities.map((activity) => (
-          <div key={activity.title} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col h-full">
+        {activities.map((activity, index) => (
+          <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col h-full">
         <Image src={activity.image} alt={activity.title} width={300} height={200} className="w-full h-48 object-cover" />
         <div className="p-4 flex flex-col flex-grow">
           <div className="flex-grow">
         <h3 className="font-bold text-black text-xl mb-2">{activity.title}</h3>
         <p className="text-black text-sm">{activity.description}</p>
           </div>
-          <Link href={`/kegiatan/${activity.title.toLowerCase()}`} className="mt-4 bg-teal-800 text-white px-4 py-2 rounded-md hover:bg-teal-700 w-full text-center">
-        Selengkapnya
+          <Link 
+              href={`/kegiatan/${activity.title.toLowerCase().replace(/\s+/g, '-')}`} 
+              className="mt-4 bg-teal-800 text-white px-4 py-2 rounded-md hover:bg-teal-700 w-full text-center"
+            >
+              Selengkapnya
           </Link>
         </div>
           </div>
